ad8d71e38439ec75640e7209d463ca1b67dcac13
XuMox <wkae2am@163.com>
2018-04-02 11:51:20 +0800

    111111

--- src/com/hr/test/Test.java
+++ src/com/hr/test/Test.java
@@ -2,6 +2,6 @@ package com.hr.test;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("aaaaaaaaaabbbb");
+        System.out.println("aaaaaaaaaabbbbccc");
     }
 }
